889c35807cf239e817b8917922c4c76e9fe972ec
kpraveend <KPRAVEEND@maxis.com.my>
2023-01-11 10:32:16 +0530

    Initial Commit

---
+++ WindowsService1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using CoreScanner;
+
+namespace WindowsService1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// 
+        static CCoreScanner cCoreScannerClass;
+        Timer timer = new Timer();
+        static void OnBarCodeEvent(short eventType, ref string scanData)
+        {
+            WriteToFile("Captured at: " + DateTime.Now);
+            WriteToFile("Data: " + scanData);
+            // Call our COPRA API
+        }
+
+        static void Main()
+        {
+            cCoreScannerClass = new CCoreScanner();
+            short[] scannerTypes = new short[1];//Scanner Types you are interested in
+            scannerTypes[0] = 1; // 1 for all scanner types
+            short numberOfScannerTypes = 1; // Size of the scannerTypes array
+            int status; // Extended API return code
+            cCoreScannerClass.Open(0, scannerTypes, numberOfScannerTypes, out status);
+            // Subscribe for barcode events in cCoreScannerClass
+            cCoreScannerClass.BarcodeEvent += new _ICoreScannerEvents_BarcodeEventEventHandler(OnBarCodeEvent);
+
+            // Let's subscribe for events
+            int opcode = 1001; // Method for Subscribe events
+            string outXML; // XML Output
+            string inXML = "<inArgs>" +
+            "<cmdArgs>" +
+            "<arg-int>1</arg-int>" +
+            "</cmdArgs>" +
+            "</inArgs>";
+            cCoreScannerClass.ExecCommand(opcode, ref inXML, out outXML, out status);
+            WriteToFile("Application Started!" + DateTime.Now);
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+
+        static void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.   
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+    }
+}
